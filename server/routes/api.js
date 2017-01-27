1feaa30270fe74e1cb04316ace3723a20ae3a4bf
MATT BRUTON <the.bruton@gmail.com>
2017-01-26 22:00:46 -0600

    added express and body-parser, initial scaffolding, created server directory, routes dir, and api route

---
+++ server/routes/api.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express'),
+    router = express.Router();
+
+/* GET api listing. */
+router.get('/', (req, res) => {
+  res.send('api works');
+});
+
+module.exports = router;
